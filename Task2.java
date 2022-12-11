a39791b953e34b69c1fae7167cf0dbaddc618792
BM <bm1120836@yandex.ru>
2022-12-11 21:36:02 +0300

    Найти сумму положительных чисел в последовательности N

---
+++ Task2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+ * Дана последовательность целых чисел, оканчивающаяся нулем. 
+ * Найти сумму положительных чисел, после которых следует отрицательное число.
+ */
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a; // текущее число
+        int b; // последующее число
+        int sum = 0; // количество положительных чисел
+        int n; // число элементов в последовательности
+         Scanner scanner = new Scanner(System.in);
+         n = scanner.nextInt();
+         a = scanner.nextInt();
+         
+         for(int i = 1; i < n; i++ ) {
+             b = scanner.nextInt();
+             if((a > 0) && (b < 0)) {
+                 sum+=a;
+             }
+             a=b;
+             
+         }
+         System.out.println("sum = " + sum);
+         scanner.close();
+         
+     }
+     
+     }
+

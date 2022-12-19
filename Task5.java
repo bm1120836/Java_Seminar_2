f3b2bf1d60b7f7094805a2ebb639a4e9035355b0
BM <bm1120836@yandex.ru>
2022-12-19 19:52:33 +0300

    Найти сумму элементов, у которых последняя и предпоследняя цифры равны

--- Task5.java
+++ Task5.java
@@ -1,44 +1,22 @@
 import java.util.Arrays;
-import java.util.Random;
-
-/*
- * Дан массив целых чисел. 
- * Найти сумму элементов, у которых последняя и предпоследняя цифры равны.
- */
-// 1. Задать массив.
-// 2. Заполнить массив случайными числами.
-// 3. Создать цикл с условием выделения чисел, у которых последняя и предпоследняя цифры равны.
-
-
-
 
 public class Task5 {
     /**
      * @param args
      * @param sum
      */
-    public static void main(String[] args, char[] sum) {
-        int[] array = { 131, 266, 30, 4755, 5 };
-        System.out.println(Arrays.toString(array));
-        //int sum = 0;
+    public static void main(String[] args) {
+        String str;
+        int sum = 0;
+        int [] array = {55,34,44,89877,60,444,81,};                   
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > 10) {
-                int num = array[i];
-                int num1 = num % 100;
-                int num2 = num1 % 10;
-                int num3 = num1 - num2;
-                int num4 = num3 / 10;
-                if (num4 == num2) {
-                    int summ = array[i];
-                }
+            str = Integer.toString(array[i]);                                         
+            if (str.charAt(str.length() - 1) == str.charAt(str.length() - 2)){      
+                if (sum == 0) System.out.printf("  %d ",array[i]);            
+                else System.out.printf(" + %d ",array[i]);                    
+                sum += array[i];
             }
-            //int sum = sum + array[i];
         }
-    System.out.println(summ);
+        System.out.printf(" = %d",sum);  
     }
-        
-            
-        }
-    
-    
-
+}
